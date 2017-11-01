9091a21074ec01b5331f80100e46d61f8833c773
ruan <ruan.andrade@funcate.org.br>
2017-11-01 09:44:25 -0200

    Simple answer happy numbers

---
+++ happynumbers.py
@@ -0,0 +1,18 @@
+def sum_of_squares(number):
+    string = str(number)
+    digits = [int(char)**2 for char in string]
+    return sum(digits)
+
+
+def happy(number):
+    box = []
+    n = number
+    while n != 1 and n not in box:
+        box.append(n)
+        n = sum_of_squares(n)
+    return n == 1
+
+
+assert sum_of_squares(130) == 10
+assert all([happy(n) for n in (1, 10, 130, 97)])
+assert not happy(4)
